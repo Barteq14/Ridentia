854dda3e55648ef0efb711781b9d55545d967b02
Bartek Longota <b.longota2@wp.pl>
2020-09-06 18:23:34 +0200

    add

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Gra_przegladarkowa.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
