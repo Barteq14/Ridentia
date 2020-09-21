67d95e91ba4a2c70a887a466f3ca083d4ef92ff8
denied323 <demrumrus1@tlen.pl>
2020-09-21 20:07:52 +0200

    Widok logowanie i rejestracji (oddzielnie)

---
+++ Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Gra_przegladarkowa.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(Gra_przegladarkowa.Areas.Identity.IdentityHostingStartup))]
+namespace Gra_przegladarkowa.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
