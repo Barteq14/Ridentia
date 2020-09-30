1b9cb52f8b873b94c0a958bed444a5ca7353b7c0
denied323 <demrumrus1@tlen.pl>
2020-09-30 18:53:35 +0200

    Concept work views with counting time

--- Controllers/CreateCharacter/WorkController.cs
+++ Controllers/CreateCharacter/WorkController.cs
@@ -1,36 +1,153 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using Gra_przegladarkowa.DAL;
 using Gra_przegladarkowa.Models.Character;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using System.Security.Claims;
+using System.Globalization;
 
 namespace Gra_przegladarkowa.Controllers.CreateCharacter
 {
     public class WorkController : Controller
     {
         private readonly RidentiaDbContext _context;
+        private readonly UserManager<IdentityUser> _userManager;
 
-        public WorkController(RidentiaDbContext context)
+        public WorkController(RidentiaDbContext context, UserManager<IdentityUser> userManager)
         {
             _context = context;
+            _userManager = userManager;
         }
 
 
-        //Get: /CreateCharacter
         [HttpGet]
         public async Task<ActionResult<IEnumerable<Work>>> Index()
         {
             var work = await _context.Works.Select(p => p).ToListAsync();
+
+            var user = await _userManager.GetUserAsync(User); //user
+            var isUserBlocked = await _userManager.IsLockedOutAsync(user); //czy locked
+
+            if (isUserBlocked == true)
+            {
+                return RedirectToAction("Work");
+                //return RedirectToPage("./Work");
+            }
+
             return View("Index", work);
         }
 
-        public IActionResult Work()
+        [HttpGet]
+        public async Task<ActionResult> Work()
         {
+
+            var user = await _userManager.GetUserAsync(User); //user
+            var isUserBlocked = await _userManager.IsLockedOutAsync(user); //czy locked
+
+
+            if (isUserBlocked == false)
+            {
+                ViewData["czyblok"] = "false ";
+
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                string phrase = "" + await _userManager.GetLockoutEndDateAsync(user);
+                string[] words = phrase.Split(' ');
+                for (int i = 0; i < words.Length; i++)
+                {
+                    System.Console.WriteLine(words[i]);
+                }
+
+                string dataNasc = words[0] + " " + words[1];
+                DateTime time = DateTime.ParseExact(dataNasc, "dd.MM.yyyy HH:mm:ss", null);
+
+                DateTime yourDate = Convert.ToDateTime(time);
+                Console.WriteLine(yourDate.ToString("MMMM dd yyyy"));
+
+                ViewData["expireDate"] = yourDate.ToString("MMMM dd yyyy", CultureInfo.InvariantCulture) + " " + words[1];
+            }
+
+            
+
             return View();
         }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken] //?
+        public async Task<ActionResult> Work(string namework, int timework)
+        {
+
+            var user = await _userManager.GetUserAsync(User); //user
+            var isUserBlocked = await _userManager.IsLockedOutAsync(user); //czy locked
+
+            
+
+            //jeżeli nie locked to ustawia jemu locked na ileś tam godzin
+            if (isUserBlocked == false)
+            {
+                DateTime localDate = DateTime.UtcNow.ToLocalTime();
+
+                //zmienić później na godziny
+                DateTime lockDate = localDate.AddMinutes(timework); //minuty
+                ViewData["lockDate"] = lockDate;
+                await _userManager.SetLockoutEndDateAsync(user, lockDate); // blokowanie
+
+                string phrase = "" + await _userManager.GetLockoutEndDateAsync(user);
+                string[] words = phrase.Split(' ');
+                for (int i = 0; i < words.Length; i++)
+                {
+                    System.Console.WriteLine(words[i]);
+                }
+                string dataNasc = words[0] + " " + words[1];
+                DateTime time = DateTime.ParseExact(dataNasc, "dd.MM.yyyy HH:mm:ss", null);
+                DateTime yourDate = Convert.ToDateTime(time);
+                Console.WriteLine(yourDate.ToString("MMMM dd yyyy"));
+                ViewData["expireDate"] = yourDate.ToString("MMMM dd yyyy", CultureInfo.InvariantCulture) + " " + words[1];
+
+                return View();
+            }
+
+
+            
+
+            return RedirectToAction("Index");
+        }
+
+
+
+        [HttpGet]
+        public async Task<ActionResult> Cancel()
+        {
+
+            var user = await _userManager.GetUserAsync(User); //user
+            var isUserBlocked = await _userManager.IsLockedOutAsync(user); //czy locked
+
+            if (isUserBlocked == false)
+            {
+                return RedirectToAction("Index");
+            }
+
+                await _userManager.SetLockoutEndDateAsync(user, new DateTimeOffset(DateTime.UtcNow));
+
+            return RedirectToAction("Index");
+        }
+
+
+
+
+
+
+
+
+
 
     }
 }
---
+++ wwwroot/js/work/work.js
@@ -0,0 +1,43 @@
+﻿// The data/time we want to countdown to
+var countDownDate = new Date(document.getElementById("expireData").innerHTML).getTime();
+
+
+// Run myfunc every second
+var myfunc = setInterval(function () {
+
+    var now = new Date().getTime();
+    var timeleft = countDownDate - now;
+    var strdays = ""; var strhours = "";
+
+    // Calculating the days, hours, minutes and seconds left
+    var days = Math.floor(timeleft / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((timeleft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((timeleft % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((timeleft % (1000 * 60)) / 1000);
+
+    // Result is output to the specific element
+    if (days > 0) {
+        document.getElementById("dateCount").innerHTML = "Praca zakończy się za " + days + " dni, " + hours + " godzin, " + minutes + " minut i " + seconds + " sekund";
+    }
+    if (days == 0 && hours > 0) {
+        document.getElementById("dateCount").innerHTML = "Praca zakończy się za " + hours + " godzin, " + minutes + " minut i " + seconds + " sekund";
+    }
+    else if (days == 0 && hours == 0 && minutes > 0) {
+        document.getElementById("dateCount").innerHTML = "Praca zakończy się za " + minutes + " minut i " + seconds + " sekund";
+    }
+    else if (days == 0 && hours == 0 && minutes == 0) {
+        document.getElementById("dateCount").innerHTML = "Praca zakończy się za " + seconds + " sekund";
+    }
+    else {
+        document.getElementById("dateCount").innerHTML = "Praca zakończy się za " + days + " dni, " + hours + " godzin, " + minutes + " minut i " + seconds + " sekund";
+    }
+
+    // Display the message when countdown is over
+    if (timeleft < 0) {
+        clearInterval(myfunc);
+        document.getElementById("dateCount").innerHTML = ""
+        window.location.href = '/Work/Index';
+    }
+
+
+}, 1000);
