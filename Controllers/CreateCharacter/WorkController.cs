11e22e8a8a9f24039eaf53a1948f700613ed837f
denied323 <demrumrus1@tlen.pl>
2020-09-30 10:17:37 +0200

    Concept Work view

--- Controllers/CreateCharacter/WorkController.cs
+++ Controllers/CreateCharacter/WorkController.cs
@@ -2,15 +2,29 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Gra_przegladarkowa.DAL;
+using Gra_przegladarkowa.Models.Character;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Gra_przegladarkowa.Controllers.CreateCharacter
 {
     public class WorkController : Controller
     {
-        public IActionResult Index()
+        private readonly RidentiaDbContext _context;
+
+        public WorkController(RidentiaDbContext context)
         {
-            return View();
+            _context = context;
+        }
+
+
+        //Get: /CreateCharacter
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Work>>> Index()
+        {
+            var work = await _context.Works.Select(p => p).ToListAsync();
+            return View("Index", work);
         }
 
         public IActionResult Work()
